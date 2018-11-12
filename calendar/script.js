9324106cf7f44e65d06806e9d3ec5ab55910f84d
PronSiman <Alex_vlasov@i.ua>
2018-11-13 01:02:18 +0200

    make calendar

--- calendar/script.js
+++ calendar/script.js
@@ -3,14 +3,34 @@
 const date = new Date;
 const yearDate = document.querySelector('.year-date');
 const mounthName = document.querySelector('.mounth-name');
-const mounthNames = ['Январь', 'Февраль', 'Март', 'Аперль', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
+const mounthNames = ['Январь', 'Февраль', 'Март', 'Аперль', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+const dayNames = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
 const arrowBack = document.querySelector('.arrow-back');
 const arrowForward = document.querySelector('.arrow-forward');
 const todayBtn = document.querySelector('.today-btn');
 const calendarBox = document.querySelector('.calendar-box');
+const dataWindow = document.querySelector('.data-window');
+const closeBtn = document.querySelector('.close-btn');
+///// inputs
+const eventInput = document.querySelector('.event-input');
+const dateOfInput = document.querySelector('.date-input');
+const membersInput = document.querySelector('.members-input');
+const descriptionInput = document.querySelector('.description-input');
+////btns 
+const submitBtn = document.querySelector('.submit-btn');
+const resetBtn = document.querySelector('.reset-btn');
+// const submitBtn = document.querySelector('.submit-btn');
+/////
 let currentMounthNum;
 let currentYearNum;
 let dataBoxes = '';
+let datesElements;
+let currentEvent;
+let keysDatesArrey = [];
+let boxData;
+let stringData;
+
+
 
 function currentDate() {
     currentYearNum = date.getFullYear();
@@ -18,8 +38,9 @@ function currentDate() {
     mounthName.textContent = getMounthName(date.getMonth());
     topArrowListener();
     creatCalendar(currentMounthNum, currentYearNum);
-    // const dateSquares = document.querySelectorAll('.date');
-    // todayActiveElement(dateSquares);
+    hideModal(); ////////
+    localDataCheker();
+
 }
 currentDate();
 
@@ -52,6 +73,8 @@ function mounthChanger(num) {
     }
     mounthName.textContent = getMounthName(currentMounthNum);
     creatCalendar(currentMounthNum, currentYearNum);
+    dateClickListener();
+    localDataCheker();
 }
 
 function getDay(date) { // получить номер дня недели, от 0(пн) до 6(вс)
@@ -61,32 +84,194 @@ function getDay(date) { // получить номер дня недели, от
 }
 
 function creatCalendar(month, year) {
-    dataBoxes = '';
+    calendarBox.innerHTML = '';
     let currentData;
+    let calendar = document.createDocumentFragment();
     currentData = new Date(year, month);
 
     for (let i = 0, max = getDay(currentData); i < max; i += 1) {
-        dataBoxes += '<div class="date"></div>'; //////
+        boxCreater();
     }
 
     while (currentData.getMonth() == month) {
-        dataBoxes += '<div class="date">' + currentData.getDate() + '</div>';
+        boxCreater(currentData.getDate())
         currentData.setDate(currentData.getDate() + 1);
     }
     if (getDay(currentData) != 0) {
-        for (var i = getDay(currentData); i < 7; i++) {
-            dataBoxes += '<div class="date"></div>';
+        for (let i = getDay(currentData); i < 7; i += 1) {
+            boxCreater();
         }
     }
-    calendarBox.innerHTML = dataBoxes;
+    calendarBox.appendChild(calendar);
+
+    function boxCreater(datenum = '') {
+        dataBoxes = document.createElement('div');
+        let dataBoxesNum = document.createElement('p');
+        let insideBox = document.createElement('div');
+        insideBox.classList.add('inside');
+        dataBoxes.classList.add('date');
+        dataBoxesNum.classList.add('date-number')
+        dataBoxesNum.textContent += datenum;
+        // dataBoxesNum.appendChild(insideBox);
+        dataBoxes.appendChild(dataBoxesNum);
+        dataBoxes.appendChild(insideBox);
+        calendar.appendChild(dataBoxes);
+
+    }
+    datesElements = document.querySelectorAll('.date');
+
+    function todayLighter() {
+        datesElements.forEach(elem => {
+            elem.classList.remove('date--active');
+            if (elem.textContent == date.getDate()) {
+                elem.classList.add('date--active');
+            }
+        })
+    }
+    todayLighter();
+
+    function dayNamesCreator(days) {
+        for (let i = 0; i < 7; i += 1) {
+            let dayName = document.createElement('p');
+            dayName.classList.add('boldtext');
+            dayName.textContent += days[i];
+            datesElements[i].prepend(dayName);
+            dayName.classList.add('dayname');
+        }
+    }
+    dayNamesCreator(dayNames);
 }
 
-// function todayActiveElement(elements) {
 
-//     elements.forEach(function(elem) {
-//         console.log(elem)
-//         if (elem.textContent == date.getDate()) {
-
-//         }
-//     })
+function dateClickListener() {
+    datesElements.forEach(box => {
+        box.addEventListener('click', showDataWindow)
+    })
+}
+// function inputUsersData(currentNum) {
 // }
+function showDataWindowClear() { //
+    eventInput.value = '';
+    dateOfInput.value = '';
+    membersInput.value = '';
+    descriptionInput.value = '';
+}
+
+function hideModal() {
+    dataWindow.hidden = true;
+}
+
+let currentNum;
+
+function showDataWindow() {
+    showDataWindowClear();
+    let currentEvent = this;
+    currentNum = currentEvent.getElementsByClassName('date-number')[0].textContent;
+    console.log(currentNum)
+    if (currentEvent.classList.contains('info')) {
+        eventInput.value = setTextContent('event-txt');
+        dateOfInput.value = setTextContent('userdates-txt');
+        membersInput.value = setTextContent('members-txt');
+        descriptionInput.value = setTextContent('desc-txt');
+
+        function setTextContent(classname) {
+            return currentEvent.getElementsByClassName(classname)[0].textContent
+        }
+    }
+    dataWindow.hidden = false;
+}
+
+function modalBtnsLissener() {
+    submitBtn.addEventListener('click', submitClick);
+    closeBtn.addEventListener('click', hideModal);
+}
+
+modalBtnsLissener();
+
+function deleteFromLocal(num) {
+    if (num < 10) {
+        num = '0' + num;
+    }
+    for (let key in localStorage) {
+        if (isNaN(key) == false) {
+            let local = JSON.parse(localStorage.getItem(key));
+            if (local.eventDate == num) {
+                localStorage.removeItem(key);
+
+            }
+        }
+
+    }
+}
+
+function submitClick() {
+    // hideModal();
+    if (eventInput.value == '' && dateOfInput.value == '' &&
+        membersInput.value == '' && descriptionInput.value == '') {
+        console.log('clear');
+        deleteFromLocal(currentNum);
+        // currentDate();
+        localDataCheker(); /////////
+        hideModal();
+        return;
+    }
+    let userInputData = {
+        evt: eventInput.value,
+        userdates: dateOfInput.value,
+        members: membersInput.value,
+        description: descriptionInput.value,
+        numCreator: function(num, n) {
+            num = num + '';
+            if (num < 10) {
+                num = '0' + num;
+            }
+            return userInputData[n] = num;
+        }
+    }
+    userInputData.numCreator(currentNum, 'eventDate');
+    userInputData.numCreator(currentMounthNum, 'eventBoxMonth');
+    userInputData.numCreator(currentYearNum, 'eventBoxYear');
+    let localKey = userInputData.eventDate +
+        userInputData.eventBoxMonth +
+        userInputData.eventBoxYear;
+    let stringData = JSON.stringify(userInputData);
+    localStorage.setItem(localKey, stringData);
+    hideModal();
+    localDataCheker();
+
+}
+
+function localDataCheker() {
+    for (let key in localStorage) {
+        if (isNaN(key) == false) {
+            getFromLocalStorage(key)
+        }
+
+    }
+}
+
+function getFromLocalStorage(key) {
+    let localIndex = JSON.parse(localStorage.getItem(key));
+
+    if (localIndex.eventDate.charAt(0) === '0') {
+        localIndex.eventDate = localIndex.eventDate.substr(1);
+    }
+    if (currentYearNum == localIndex.eventBoxYear) {
+        if (currentMounthNum == localIndex.eventBoxMonth) {
+            datesElements.forEach(i => {
+
+                let dateNumber = i.getElementsByClassName('date-number')[0];
+                // console.log(dateNumber)
+                if (dateNumber.textContent == localIndex.eventDate) {
+                    dateNumber.parentNode.classList.add('info');
+                    boxData = `<p class='event-txt'>${localIndex.evt}</p>
+                               <p class='userdates-txt'>${localIndex.userdates}</p> 
+                               <p class='members-txt'>${localIndex.members}</p>
+                               <p class='desc-txt'>${localIndex.description}</p>`;
+                    dateNumber.parentNode.children[1].innerHTML += boxData;
+                }
+            })
+        }
+    }
+}
+dateClickListener();
