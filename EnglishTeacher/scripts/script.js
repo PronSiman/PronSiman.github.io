242493bd76beacefba3ac26afa5acfd3bbed7070
PronSiman <Alex_vlasov@i.ua>
2018-12-28 10:05:20 +0200

    start page

--- EnglishTeacher/scripts/script.js
+++ EnglishTeacher/scripts/script.js
@@ -4,6 +4,9 @@ const menuIcon = document.querySelector('.menu-icon');
 const navList = document.querySelector('.nav-list');
 const arrowIcon = document.querySelector('.arrow-icon');
 const sloganTitle = document.querySelector('.slogan-title');
+const workTitles = document.querySelectorAll('.work-title');
+const portfolioTitle = document.querySelector('.portfolio-title');
+const teamTitle = document.querySelector('.team-title');
 
 const pinkColor = '#ff6666'
 
@@ -11,8 +14,7 @@ const pinkColor = '#ff6666'
 
 function lissenMenu() {
     menuIcon.addEventListener('click', () => {
-        navList.style = `display:block `
-
+        navList.style = `display:block `;
     })
 
     arrowIcon.addEventListener('click', () => {
@@ -20,13 +22,18 @@ function lissenMenu() {
     })
 }
 
+lissenMenu();
+
 function makeColorPoint(str, color) {
     const strTxt = str.textContent.split('');
     strTxt[strTxt.length - 1] = `<span style='color:${color}'>${strTxt[strTxt.length - 1]}</span>`;
     return strTxt.join('')
 }
 
+workTitles.forEach(e => {
+    e.innerHTML = makeColorPoint(e, pinkColor);
+})
 
-
-lissenMenu();
-sloganTitle.innerHTML = makeColorPoint(sloganTitle, pinkColor)
+sloganTitle.innerHTML = makeColorPoint(sloganTitle, pinkColor);
+portfolioTitle.innerHTML = makeColorPoint(portfolioTitle, pinkColor);
+teamTitle.innerHTML = makeColorPoint(teamTitle, pinkColor);
