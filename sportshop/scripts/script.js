e2b3861880ac4e0211ea1eb3faf3109ce2786853
PronSiman <Alex_vlasov@i.ua>
2019-02-07 18:13:24 +0200

    make slider

--- sportshop/scripts/script.js
+++ sportshop/scripts/script.js
@@ -1,1 +1,30 @@
 'use strict'
+
+const sliderToleftBtn = document.querySelector('.left-slider-btn-js');
+const sliderToRightBtn = document.querySelector('.right-slider-btn-js');
+const sliderList = document.querySelector('.slider-list-js');
+
+function sliderMove() {
+    sliderToleftBtn.addEventListener('click', changeSliderItemPositionToLeft);
+    sliderToRightBtn.addEventListener('click', changeSliderItemPositionToRight);
+    let currentLeft = 0;
+
+    function changeSliderItemPositionToRight() {
+        currentLeft = currentLeft + 300;
+        if (currentLeft > 0) {
+            currentLeft = 0;
+            return;
+        }
+        sliderList.style.left = currentLeft + 'px';
+    }
+
+    function changeSliderItemPositionToLeft() {
+        currentLeft = currentLeft - 300;
+        if (currentLeft < -900) {
+            currentLeft = -900;
+            return;
+        }
+        sliderList.style.left = currentLeft + 'px';
+    };
+}
+sliderMove()
