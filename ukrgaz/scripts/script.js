f2aa9e505d934633323982b15a2f409284ebb916
PronSiman <Alex_vlasov@i.ua>
2019-02-19 14:28:27 +0200

    make bottom menu

--- ukrgaz/scripts/script.js
+++ ukrgaz/scripts/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+$(document).ready(function() {
+    $('.news-slider-list').slick({
+        accessibility: false,
+        autoplay: true,
+        prevArrow: '<button type="button ">&lt;</button>',
+        nextArrow: '<button type="button ">&gt;</button>',
+    });
+
+});
