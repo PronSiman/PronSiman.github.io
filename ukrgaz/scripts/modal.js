850833755b80c7bfaef9b7f8cfe26d43cab80429
PronSiman <Alex_vlasov@i.ua>
2019-02-20 12:01:57 +0200

    make js functional for modal

--- ukrgaz/scripts/modal.js
+++ ukrgaz/scripts/modal.js
@@ -1,1 +1,20 @@
 'use strict';
+console.log('fff')
+const popBtn = document.querySelector('.pop-btn-js');
+const modal = document.querySelector('.modal');
+const closePopupBtn = document.querySelector('.close-popup-btn-js');
+
+function showModal() {
+    popBtn.addEventListener('click', () => {
+
+        modal.style.display = 'block';
+    })
+}
+
+function closeModal() {
+    closePopupBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    })
+}
+showModal();
+closeModal();
