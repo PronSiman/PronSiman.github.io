676d698edc059abcaef79df026f2253fdad5970f
PronSiman <Alex_vlasov@i.ua>
2019-02-21 23:48:16 +0200

    make slider

--- ukrgaz/scripts/slider.js
+++ ukrgaz/scripts/slider.js
@@ -2,52 +2,60 @@
 
 $(document).ready(function() {
     $('.news-slider-list').slick({
-        // slidesToShow: 3,
-        slidesToShow: 1,
+        slidesToShow: 4,
         accessibility: false,
-        autoplay: true,
+        // autoplay: true,
         pauseOnFocus: true,
         prevArrow: '<button type="button ">&lt;</button>',
         nextArrow: '<button type="button ">&gt;</button>',
-        // mobileFirst: true,
-        // responsive: [{
-        //         breakpoint: 1024,
-        //         settings: {
-        //             slidesToShow: 4,
-        //             slidesToScroll: 4,
-        //         }
-        //     }, ]
-        //     {
-        //         breakpoint: 768,
-        //         settings: {
-        //             slidesToShow: 2,
-        //             slidesToScroll: 2
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 320,
-        //         settings: {
-        //             slidesToShow: 1,
-        //             slidesToScroll: 1
-        //         }
-        //     }
-        // ],
-        // responsive: [{
-        //     breakpoint: 320,
-        //     settings: {
-        //         slidesToShow: 1,
-        //         slidesToScroll: 1
-        //     },
-        //     breakpoint: 768,
-        //     settings: {
-        //         slidesToShow: 2,
-        //         slidesToScroll: 2
-        //     },
-        //     breakpoint: 992,
-        //     settings: {
-        //         slidesToShow: 3,
-        //         slidesToScroll: 3
-        //     }
-        // }],
+
+        responsive: [{
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                    }
+                },
+                {
+                    breakpoint: 568,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+            ]
+            //     
+            //     {
+            //         breakpoint: 320,
+            //         settings: {
+            //             slidesToShow: 1,
+            //             slidesToScroll: 1
+            //         }
+            //     }
+            // ],
+            // responsive: [{
+            //     breakpoint: 320,
+            //     settings: {
+            //         slidesToShow: 1,
+            //         slidesToScroll: 1
+            //     },
+            //     breakpoint: 768,
+            //     settings: {
+            //         slidesToShow: 2,
+            //         slidesToScroll: 2
+            //     },
+            //     breakpoint: 992,
+            //     settings: {
+            //         slidesToShow: 3,
+            //         slidesToScroll: 3
+            //     }
+            // }],
     });
 });
